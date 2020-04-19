427f9f49045aea4086ffa0a97d5d885d229e31f6
Logan <im.lizhh@gmail.com>
2020-04-19 22:15:44 +0800

    learn: 第九章-排序-选择排序增加测试用例

--- 大话数据结构/第九章-排序/example/9.5-select-sort.c
+++ 大话数据结构/第九章-排序/example/9.5-select-sort.c
@@ -1,4 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "9.1-sort.h"
+
+int main(void)
+{
+  SqList * L = (SqList *)malloc(sizeof(SqList));
+  memcpy(L->r, (int []){8, 6, 4, 3, 5, 9, 1, 7, 2}, sizeof(int) * 9);
+  L->length = 9;
+  puts("Before SelectSort:");
+  printArr(L);
+  SelectSort(L);
+  puts("After SelectSort:");
+  printArr(L);
+  free(L);
+  return 0;
+}
 
 void SelectSort(SqList * L)
 {
@@ -6,8 +24,8 @@ void SelectSort(SqList * L)
   for (i = 0; i < L->length - 1; i ++)
   {
     min = i;
-    for (j = i + 1; j < L->length - 1; j++)
-      if (L->r[min] > L->r[j])
+    for (j = i + 1; j < L->length; j++)
+      if (L->r[j] < L->r[min])
         min = j;
     if (min != i)
       swap(L, i, min);
