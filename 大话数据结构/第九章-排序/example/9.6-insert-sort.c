360a07282fc3400b5a5f0f029846835b3bb90839
Logan <im.lizhh@gmail.com>
2020-04-20 14:32:55 +0800

    learn: 第九章-排序-插入排序增加测试用例

--- 大话数据结构/第九章-排序/example/9.6-insert-sort.c
+++ 大话数据结构/第九章-排序/example/9.6-insert-sort.c
@@ -1,4 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "9.1-sort.h"
+
+int main(void)
+{
+  SqList * L = (SqList *)malloc(sizeof(SqList));
+  memcpy(L->r, (int []){8, 6, 4, 3, 5, 9, 1, 7, 2}, sizeof(int) * 9);
+  L->length = 9;
+  puts("Before InsertSort:");
+  printArr(L);
+  InsertSort(L);
+  puts("After InsertSort:");
+  printArr(L);
+  free(L);
+  return 0;
+}
 
 void InsertSort(SqList * L)
 {
