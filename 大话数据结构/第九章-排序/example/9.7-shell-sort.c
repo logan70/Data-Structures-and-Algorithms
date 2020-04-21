d3b369a0348b62780e3503aa715f982cad8cf006
Logan <im.lizhh@gmail.com>
2020-04-21 19:02:22 +0800

    learn: 第九章-排序-希尔排序增加测试用例

--- 大话数据结构/第九章-排序/example/9.7-shell-sort.c
+++ 大话数据结构/第九章-排序/example/9.7-shell-sort.c
@@ -1,4 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "9.1-sort.h"
+
+int main(void)
+{
+  SqList * L = (SqList *)malloc(sizeof(SqList));
+  memcpy(L->r, (int []){8, 6, 4, 3, 5, 9, 1, 7, 2}, sizeof(int) * 9);
+  L->length = 9;
+  puts("Before ShellSort:");
+  printArr(L);
+  ShellSort(L);
+  puts("After ShellSort:");
+  printArr(L);
+  free(L);
+  return 0;
+}
 
 void ShellSort(SqList * L)
 {
@@ -11,7 +29,7 @@ void ShellSort(SqList * L)
     {
       temp = L->r[i];
       j = i - gap;
-      while (j >= 0 && L->r[j] < temp)
+      while (j >= 0 && L->r[j] > temp)
       {
         L->r[j + gap] = L->r[j];
         j -= gap;
